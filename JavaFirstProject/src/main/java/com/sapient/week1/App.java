f6a5b6909012be123ce12b2ddb9c3ba9a607bd05
Jyotsana Mall <jyotsanamall12@gmail.com>
2020-05-21 13:00:22 +0530

    adding CheckEven and test

---
+++ JavaFirstProject/src/main/java/com/sapient/week1/App.java
@@ -0,0 +1,19 @@
+package com.sapient.week1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//        System.out.println( "Hello World!" );
+    }
+}
+class CheckEven {
+    public Boolean check(int n) {
+        return n % 2 == 0;
+    }
+}
+
